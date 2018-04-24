6c363948af352860399c495569c531fc6ede83db
mercedesb <mercedesbernard@live.com>
2018-04-24 17:18:54 -0500

    use ethereal email for testing

--- routes/utilities/sendMail.js
+++ routes/utilities/sendMail.js
@@ -1,41 +1,45 @@
-const nodemailer = require("nodemailer");
+const nodemailer = require("nodemailer")
 
-/*
-    Here we are configuring our SMTP Server details.
-    STMP is mail server which is responsible for sending and recieving email.
-*/
-const smtpTransport = nodemailer.createTransport({
-    service: "gmail",
-    host: "smtp.gmail.com",
-    auth: {
-        user: "",
-        pass: ""
-    }
-});
+// should define SMTP transport here
 
 module.exports = (req, res) => {
-  //res.status(200).json({ message: 'Mail Sent!' });
+  console.log('code reached')
   if (!process.env.TO_EMAIL) {
     res.status(500).json({error: "I'm so sorry, I could not send your email at this time"})
     return
   }
-  var mailOptions={
-    to: process.env.TO_EMAIL,
-    from: req.query.from,
-    subject: req.query.subject,
-    text  req.query.text
-  }
 
-  console.log(mailOptions)
+  // Generate test SMTP service account from ethereal.email
+  // Only needed if you don't have a real mail account for testing
+  nodemailer.createTestAccount((err, account) => {
+    // create reusable transporter object using the default SMTP transport
+    const smtpTransport = nodemailer.createTransport({
+      host: 'smtp.ethereal.email',
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+          user: account.user, // generated ethereal user
+          pass: account.pass // generated ethereal password
+      }
+    })
 
-  smtpTransport.sendMail(mailOptions, function(error, response){
-    if(error){
-      console.log(error)
-      res.status(500).json({error: error})
+    // setup email data with unicode symbols
+    const mailOptions={
+      to: "mercedesrbernard@gmail.com",
+      from: req.query.from,
+      subject: req.query.subject,
+      text:  req.query.text
     }
-    else {
-      console.log("Message sent: " + response.message)
-      res.status(200.json({success: true}))
-    }
+
+     smtpTransport.sendMail(mailOptions, function(error, response){
+      if(error){
+        console.log(error)
+        res.status(500).json({error: error})
+      }
+      else {
+        console.log("Message sent: " + response.message)
+        res.status(200).json({success: true})
+      }
+    })
   })
 }
