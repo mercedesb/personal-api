dba4731e7f259c0321a4654064f82f137e7c67e2
mercedesb <mercedesbernard@live.com>
2018-04-29 09:59:23 -0500

    updated how smtp options are built

--- routes/utilities/sendMail.js
+++ routes/utilities/sendMail.js
@@ -1,12 +1,33 @@
 const nodemailer = require("nodemailer")
 
+getSmtpOptions = (account) => {
+  let auth
+
+  if (account) {
+    auth = {
+      user: account.user,
+      pass: account.pass
+    }
+  }
+  else {
+    auth = {
+      user: process.env.MAILER_USERNAME,
+      pass: process.env.MAILER_PASSWORD
+    }
+  }
+  return {
+    host: process.env.MAILER_HOST,
+    port: process.env.MAILER_PORT,
+    secure: process.env.MAILER_SECURE === 'true',
+    auth
+  }
+}
+
 // should define SMTP transport here
 getTransport = () => {
   let smtpTransport
 
   return new Promise(function(resolve, reject) {
-    debugger
-
     if (process.env.NODE_ENV == 'development') {
       nodemailer.createTestAccount((err, account) => {
         if (err) {
@@ -14,36 +35,19 @@ getTransport = () => {
         }
         else {
           // create reusable transporter object using the default SMTP transport
-          smtpTransport = nodemailer.createTransport({
-            host: 'smtp.ethereal.email',
-            port: 587,
-            secure: false, // true for 465, false for other ports
-            auth: {
-                user: account.user, // generated ethereal user
-                pass: account.pass // generated ethereal password
-            }
-          })
+          smtpTransport = nodemailer.createTransport(getSmtpOptions(account))
           resolve(smtpTransport)
         }
       })
     }
     else if (process.env.NODE_ENV == 'production') {
-      smtpTransport = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: true, // use SSL
-        auth: {
-            user: process.env.MAILER_USERNAME,
-            pass: process.env.MAILER_PASSWORD
-        }
-      })
+      smtpTransport = nodemailer.createTransport(getSmtpOptions())
       resolve(smtpTransport)
     }
   })
 }
 
 module.exports = (req, res) => {
-  debugger
   if (!process.env.TO_EMAIL) {
     res.status(500).json({error: "I'm so sorry, I could not send your email at this time"})
     return
