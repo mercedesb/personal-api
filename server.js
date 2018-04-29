e2260c0726f4ade368b2bc0144d22f7d338693f8
mercedesb <mercedesbernard@live.com>
2018-04-29 11:00:49 -0500

    allow cors

--- server.js
+++ server.js
@@ -1,16 +1,39 @@
 require('dotenv').load();
+const isDevelopment = process.env.NODE_ENV === 'development'
+const cors = require('cors')
 
 var app = require('express')()
 
-const routes = require('./routes')
+let corsOrigin
+if (isDevelopment) {
+  corsOrigin = '*'
+} else {
+  corsOrigin = function (origin, callback) {
+    const whitelist = process.env.CORS_WHITELIST.split(',')
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
 
+const corsOptions = {
+  origin: corsOrigin,
+  optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions))
+app.options('*', cors(corsOptions))
+
+const routes = require('./routes')
 app.use('/', routes)
 
 // Start server
 var port = process.env.PORT || 8090
 
 app.listen(port, () => {
-  if (process.env.NODE_ENV === 'development') {
+  if (isDevelopment) {
     console.log('Magic happens on port ' + port)
   }
 })
