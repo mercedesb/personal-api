11e24c7113d6844c671a209c65c6bf8695204ac1
mercedesb <mercedesbernard@live.com>
2018-06-10 20:20:58 -0500

    console.log for prod troubleshooting

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ if (isDevelopment) {
     console.log(origin)
     const whitelist = process.env.CORS_WHITELIST.split(',')
     if (origin === undefined || whitelist.indexOf(origin) !== -1) {
+      console.log('cors success')
       callback(null, true)
     } else {
       callback(new Error('Not allowed by CORS'))
